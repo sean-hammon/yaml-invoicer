5790077777ad309f85bbe173aa31a02608561ec4
Sean Hammon <git@sean-hammon.com>
2017-05-07 11:30:56 -0600

    Change variables to use config object.

--- invoicer/main.py
+++ invoicer/main.py
@@ -19,10 +19,10 @@ def main(include_recurring):
     invoices = datafiles.read(include_recurring)
     for invoice in invoices:
         print("Processing invoice {} for {}".format(config['next_invoice'], invoice['client']['company']['name']))
-        invoice["number"] = next_invoice_num
+        invoice["number"] = config['next_invoice']
         pdf_path = pdffiles.generate_pdf(invoice)
         mailer.send(invoice, pdf_path)
-        next_invoice_num += 1
+        config['next_invoice'] += 1
 
     with io.open("./invoice_num.txt", "w") as fh:
         fh.write(str(next_invoice_num))
